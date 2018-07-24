c3bc9afe019dbd354a83710d1a853952f19dd2f0
jing0016 <32021491+jing0016@users.noreply.github.com>
2018-07-24 10:17:29 -0400

    commit

--- links/resources/views/welcome.blade.php
+++ links/resources/views/welcome.blade.php
@@ -1,93 +1,30 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <div class="container">
+        @if(Auth::check())
+            <h2>Links List</h2>
+            <a href="/link" class="btn btn-primary">Add new link</a>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Url</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    @foreach($links as $link)
-                        <a href="{{$link->url}}">{{$link->title}}</a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+                <tbody>@foreach($user->links as $link)
+                    <tr>
+                        <td>{{$link->title}}</td>
+                        <td>{{$link->url}}</td>
+                        <td>{{$link->description}}</td>
+                        <td>
+                            <form action=""></form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
